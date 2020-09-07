18b80dd4a371d74b9ac0f769ebea4259f8ca9814
Stefani <stefani.kostic@pelagus-it.com>
2020-09-07 14:16:14 +0200

    Created background location service and added time picker

--- app/src/main/java/com/example/mpip/freeride/ClientMainActivity.java
+++ app/src/main/java/com/example/mpip/freeride/ClientMainActivity.java
@@ -1,176 +1,90 @@
 package com.example.mpip.freeride;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.PendingIntent;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
 import android.content.*;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.preference.PreferenceManager;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
-import android.widget.Toast;
 
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.*;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.example.mpip.freeride.domain.Bike;
 import com.example.mpip.freeride.domain.BikeDistance;
-import com.example.mpip.freeride.service.Common;
 import com.example.mpip.freeride.service.LocationService;
 import com.example.mpip.freeride.service.SendLocationToActivity;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
-import com.karumi.dexter.listener.single.PermissionListener;
-import com.parse.FindCallback;
-import com.parse.GetDataCallback;
-import com.parse.ParseAnalytics;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
+import com.parse.*;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.*;
 
 public class ClientMainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
-    private LocationService locationService;
-    private boolean mBound = false;
-    private final ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder iBinder) {
-            LocationService.LocalBinder binder = (LocationService.LocalBinder)iBinder;
-            locationService = binder.getService();
-            mBound = true;
-        }
 
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            locationService = null;
-            mBound = false;
-        }
-    };
     private double myLat;
     private double myLong;
 
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
-    private Database db;
+
     private ArrayList<BikeDistance> bikes = new ArrayList<BikeDistance>();
-    private ArrayList<Bitmap> bitmaps = new ArrayList<Bitmap>();
     private ArrayList<Bike> onlyBikes = new ArrayList<>();
     private GridView gridView;
     private FloatingActionButton fab;
-    private ArrayList<String> bikes1 = new ArrayList<String>();
-    public void updateLongLat(double lat, double lng) throws FileNotFoundException {
-        myLat = lat;
-        myLong = lng;
-        convertBikes();
 
-    }
 
+    @TargetApi(29)
+    @RequiresApi(api = 29)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
-
-        Dexter.withActivity(this)
-                .withPermissions(Arrays.asList(
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION))
-                                .withListener(new MultiplePermissionsListener() {
-                                    @Override
-                                    public void onPermissionsChecked(MultiplePermissionsReport report) {
-                                        new Handler().postDelayed(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                locationService.requestLocationUpdates();
-                                            }
-                                        }, 2000);
-                                        bindService(new Intent(ClientMainActivity.this, LocationService.class),
-                                                mServiceConnection,
-                                                Context.BIND_AUTO_CREATE);
-                                    }
-
-                                    @Override
-                                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
-
-                                    }
-                                }).check();
-
-
+        if(ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    ClientMainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION
+            );
+        } else {
+            startLocationService();
+        }
 
 
         gridView=(GridView) findViewById(R.id.gridview_bikes1);
         fab = findViewById(R.id.fab);
         setSupportActionBar(toolbar);
-        final ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Bike");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null) {
-                  if (objects.size() > 0) {
-                      for (ParseObject o : objects) {
-                          String id = o.getObjectId();
-                          String name = o.getString("name");
-                          int price = o.getInt("price");
-                          String category_id = o.getString("category_id");
-                          double latitude = o.getDouble("latitude");
-                          boolean rented = o.getBoolean("rented");
-                          double longitude = o.getDouble("longitude");
-                          String renter_id = o.getString("renter_id");
-                          com.example.mpip.freeride.domain.Location location = new com.example.mpip.freeride.domain.Location(latitude, longitude);
-                          ParseFile img = (ParseFile) o.get("image");
-                          try {
-                              assert img != null;
-                              Bitmap bitmap = BitmapFactory.decodeByteArray(img.getData(), 0, img.getData().length);
-                              Bike bike = new Bike(id, name, price, bitmap, rented, location, renter_id, category_id);
-                              onlyBikes.add(bike);
-                          } catch (ParseException ex) {
-                              ex.printStackTrace();
-                          }
-                      }
-                  }
-                } else {
-                    e.printStackTrace();
-                }
-            }
-                    });
-
-
-
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.MANAGE_DOCUMENTS},
@@ -184,43 +98,18 @@ public class ClientMainActivity extends AppCompatActivity implements SharedPrefe
                         .setAction("Action", null).show();
             }
         });
-
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .registerOnSharedPreferenceChangeListener(this);
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        if(mBound) {
-            unbindService(mServiceConnection);
-            mBound = false;
-        }
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(this);
-        EventBus.getDefault().unregister(this);
-        super.onStop();
-
     }
 
     @Override
     public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted.
-                try {
-                    convertBikes();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            } else {
+                startLocationService();
+
+            }else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
                 // User refused to grant permission.
             }
         }
@@ -278,6 +167,69 @@ public class ClientMainActivity extends AppCompatActivity implements SharedPrefe
            myLat = event.getLocation().getLatitude();
            myLong = event.getLocation().getLongitude();
            convertBikes();
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if(activityManager != null){
+            for(ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if (LocationService.class.getName().equals(service.service.getClassName())) {
+                    if (service.foreground) {
+                        return true;
+                    }
+                }
+            }
+                return false;
+            }
+            return false;
+    }
+
+    private void startLocationService() {
+        if(!isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service started", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopLocationService(){
+        if(isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static class Task extends AsyncTask<Void, Void, List<Object>> {
+        private WeakReference<Context> mContextRef;
+        public Task(Context context) {
+            mContextRef = new WeakReference<>(context);
+            //later when you need your context just use the 'get()' method. like : mContextRef.get() (this will return a Context Object.
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // show progress Dialog
+            //this method is processed in the MainThread, though it can prepare data from the background thread.
+        }
+
+        @Override
+        protected List<Object> doInBackground(Void ... params) {
+            List<Object> mList = new ArrayList<>();
+            //Call your content provider here and gather the cursor and process your data..
+            //return the list of object your want to show on the MainThread.
+            return mList;
+        }
+
+        @Override
+        protected void onPostExecute(List<Object> list) {
+            if(list.size() > 0) {
+                //do your stuff : i.e populate a listView
+            }
         }
     }
 }
