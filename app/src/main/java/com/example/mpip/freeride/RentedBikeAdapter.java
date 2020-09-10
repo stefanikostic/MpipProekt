d70af3921fe2c99779871190f96af7205d3788fd
Stefani <stefani.kostic@pelagus-it.com>
2020-09-10 22:11:08 +0200

    Added broadcast receivers for notifications and deleting rent after the time finishes

--- app/src/main/java/com/example/mpip/freeride/RentedBikeAdapter.java
+++ app/src/main/java/com/example/mpip/freeride/RentedBikeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mpip.freeride;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +10,21 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.mpip.freeride.domain.Bike;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.parse.*;
 
 public class RentedBikeAdapter extends BaseAdapter {
     private Context mContext;
     private final Bike[] bikes;
+    private final String[] rent_ids;
     String clientId;
     LayoutInflater inflter;
 
-    public RentedBikeAdapter(Context mContext, Bike[] bikes, String clientId){
+    public RentedBikeAdapter(Context mContext, Bike[] bikes, String[] rent_ids, String clientId){
         this.mContext = mContext;
         this.bikes = bikes;
         this.clientId = clientId;
+        this.rent_ids = rent_ids;
         inflter = (LayoutInflater.from(mContext));
     }
 
@@ -43,19 +48,45 @@ public class RentedBikeAdapter extends BaseAdapter {
         LayoutInflater layoutInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View listing = layoutInflater.inflate(R.layout.rented_gridview_item, parent, false);
         Bitmap bitmap = bikes[position].getImage();
-        ImageView icon = (ImageView) listing.findViewById(R.id.icon); // get the reference of ImageView
+        final ImageView icon = (ImageView) listing.findViewById(R.id.icon); // get the reference of ImageView
         icon.setImageBitmap(bitmap); // set logo images
         TextView textView = (TextView) listing.findViewById(R.id.textView);
         textView.setText(bikes[position].getName());
-/*        listing.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) listing.findViewById(R.id.cancelBike);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(mContext, AddBicycleActivity.class);
-                i.putExtra("id", bikes[position].getId());
-                i.putExtra("bikeExists", "true");
-                    mContext.startActivity(i);
+                final ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Bike");
+                query.whereEqualTo("objectId", bikes[position].getId());
+                query.getFirstInBackground(new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject parseObject, ParseException e) {
+                        parseObject.put("rented", false);
+                        parseObject.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Rents");
+                                query1.whereEqualTo("objectId", rent_ids[position]);
+                                query1.getFirstInBackground(new GetCallback<ParseObject>() {
+                                    @Override
+                                    public void done(ParseObject obj, ParseException e) {
+                                        obj.deleteInBackground(new DeleteCallback() {
+                                            @Override
+                                            public void done(ParseException e) {
+                                                Intent intent = new Intent(mContext, RentedBikesActivity.class);
+                                                intent.putExtra("client_id", clientId);
+                                                mContext.startActivity(intent);
+                                            }
+                                        });
+
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
             }
-        });*/
+        });
         return listing;
     }
 }
